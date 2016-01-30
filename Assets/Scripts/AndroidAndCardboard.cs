17cdd3fe0e6e6a44382605b7adce49acccf403c5
Jani Moilanen <jani.moil@gmail.com>
2016-01-30 16:54:09 +0200

    Universal Object has android settings. Head Main adjusted to zero. Project Settings for android is locked to landscape.

--- Assets/Scripts/AndroidAndCardboard.cs
+++ Assets/Scripts/AndroidAndCardboard.cs
@@ -3,6 +3,11 @@ using System.Collections;
 
 public class AndroidAndCardboard : MonoBehaviour
 {
+    void Awake()
+    {
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+    }
+
     void LateUpdate()
     {
         Cardboard.SDK.UpdateState();
