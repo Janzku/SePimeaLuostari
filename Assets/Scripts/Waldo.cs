ba696503267edd5469845cdec0b72efed268b57c
unknown <tonikui@AOTA1-1LDML-31D.kamit.fi>
2016-01-30 20:59:42 +0200

    First implementation of trial_waldo.

---
+++ Assets/Scripts/Waldo.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Waldo : BaseBehaviour
+{
+    public bool waldoFound = false;
+    private CardboardAudioSource AS;
+
+    void Start()
+    {
+        AS = GetComponent<CardboardAudioSource>();
+        AS.volume = 0;
+    }
+
+    void Update()
+    {
+        PlayerLookCheck();
+    }
+
+    void PlayerLookCheck()
+    {
+        if (PlayerLooking(60))
+        {
+            var look = Camera.main.transform.forward;
+            var pos = transform.position;
+            var ang = Vector3.Angle(look, pos);
+            float volumeMultiplier = 60 - ang;
+            AS.volume = 1.0f / 60.0f * volumeMultiplier;
+            if (PlayerLooking(3))
+            {
+                MakeProgress();
+            }
+            else
+            {
+                ResetProgress();
+            }
+        }
+    }
+
+    void MakeProgress()
+    {
+        AS.pitch = AS.pitch + 0.05f;
+
+        if (AS.pitch >= 5.0f)
+        {
+            FoundWaldo();
+        }
+    }
+
+    void ResetProgress()
+    {
+        AS.pitch = 1.0f;
+    }
+
+    void FoundWaldo()
+    {
+        Debug.Log("you found waldo!");
+        Destroy(this.gameObject);
+    }
+
+    void OnDestroy()
+    {
+
+    }
+}
---
+++ Assets/Scripts/WaldoSpawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaldoSpawner : MonoBehaviour
+{
+    public GameObject WaldoPrefab = null;
+
+    private GameObject m_waldo = null;
+
+
+    void Update()
+    {
+        //Debug.Log("Waldo: " + m_waldo);
+        if (m_waldo == null)
+        {
+            float randomDirection = Random.Range(-180.0f, 180.0f);
+            int spawnDistance = 5;
+            m_waldo = Instantiate(WaldoPrefab);
+            m_waldo.transform.SetParent(this.gameObject.transform);
+            m_waldo.transform.position = new Vector3(0, 0, spawnDistance);
+            m_waldo.transform.RotateAround(Vector3.zero, Vector3.up, randomDirection);
+            m_waldo.transform.LookAt(new Vector3(0, m_waldo.transform.position.y, 0));
+        }
+    }
+}
