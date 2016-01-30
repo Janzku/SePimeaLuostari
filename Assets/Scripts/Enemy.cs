4748f1ac6730d5410b3dc386a74f92624bfbce9d
unknown <tonikui@AOTA1-1LDML-31D.kamit.fi>
2016-01-30 17:18:22 +0200

    First implementation of many monks charging at you -trial.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : BaseBehaviour
+{
+    private int consecutiveLookedAtFrames = 0;
+    private bool lookedAt = false;
+
+    void Update()
+    {
+        if (PlayerLooking(10))
+        {
+            consecutiveLookedAtFrames++;
+            lookedAt = true;
+        }
+        else
+        {
+            consecutiveLookedAtFrames = 0;
+            lookedAt = false;
+        }
+        DestroyCheck();
+        Move();
+        KillCheck();
+    }
+
+    void KillCheck()
+    {
+        if (transform.position.x <= 0.5f && transform.position.x >= -0.5f || transform.position.z <= 0.5f && transform.position.z >= -0.5f)
+        {
+            Debug.Log("DOD");
+            Destroy(this.gameObject);
+        }
+    }
+
+    void DestroyCheck()
+    {
+        if (consecutiveLookedAtFrames >= 60)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void Move()
+    {
+        if (lookedAt)
+        {
+            transform.Translate(Vector3.back * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject EnemyPrefab = null;
+    private int spawnTimer = 0;
+
+    void Update()
+    {
+        // if(Input.GetKeyDown(KeyCode.S))
+        if (spawnTimer == 180)
+        {
+            GameObject _go = Instantiate(EnemyPrefab);
+            _go.transform.SetParent(this.gameObject.transform);
+            _go.transform.position = new Vector3(Random.Range(-15, 15), 0, 5);
+            _go.transform.LookAt(new Vector3(0, _go.transform.position.y, 0));
+
+            spawnTimer = 0;
+        }
+        else
+        {
+            spawnTimer++;
+        }
+    }
+}
