68a9ff20d0a828321fe9406919c63d62347b3c5c
Eracage <eracage@gmail.com>
2016-01-30 16:35:58 +0200

    Added PlayerLooking(float angle, bool ignoreHeight = true) function to BaseBehaviour.cs

--- Assets/Scripts/BaseBehaviour.cs
+++ Assets/Scripts/BaseBehaviour.cs
@@ -18,4 +18,23 @@ public class BaseBehaviour : MonoBehaviour
         GetComponent<Renderer>().material.color = gazedAt ? Color.green : Color.red;
         isPlayerLooking = gazedAt;
     }
+
+    public bool PlayerLooking(float angle, bool ignoreHeight = true)
+    {
+        if (isPlayerLooking)
+            return true;
+
+        var look = Camera.main.transform.forward;
+        var pos = transform.position;
+
+        if (ignoreHeight)
+        {
+            look.y = 0;
+            pos.y = 0;
+        }
+
+        var ang = Vector3.Angle(Camera.main.transform.forward, transform.position);
+
+        return ang < angle;
+    }
 }
