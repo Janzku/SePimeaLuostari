31b96ec58182c14e50f2861e0a3efccf8c01913b
unknown <AleksiJok@AOTA1-1LDML-21D.kamit.fi>
2016-01-30 18:03:58 +0200

    Edited Directions.

---
+++ Assets/direction.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class direction : BaseBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerLooking(50, false))
+        {
+            GetComponent<CardboardAudioSource>().mute = false;
+        }
+        else
+        {
+            GetComponent<CardboardAudioSource>().mute = true;
+        }
+ 
+        
+    }
+}
