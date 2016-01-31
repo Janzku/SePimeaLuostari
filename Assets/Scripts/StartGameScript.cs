cdbc77ab60a193b4c85b6bd77695e94979ffe7d5
Jani Moilanen <jani.moil@gmail.com>
2016-01-31 14:20:52 +0200

    Initial scene to equip head mount and start game by looking up.

---
+++ Assets/Scripts/StartGameScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGameScript : MonoBehaviour
+{
+    private GameObject Player = null;
+
+    private float m_timer = 3.0f;
+    private bool m_beginGame = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Player = Camera.main.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Player.transform.rotation.x <= -0.4f)
+        {
+            Debug.Log("Beginning game");
+            m_beginGame = true;
+        }
+
+        if(m_beginGame)
+        {
+            m_timer -= Time.deltaTime;
+
+            if(m_timer <= 0.0f)
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene("Intro");
+            }
+        }
+    }
+}
